bbc45af3fb767e1b907e54ede5c6346f0df5acee
DoğukanKazan <kazand25@gmail.com>
2021-08-11 13:11:45 +0300

    Kurumsal-Mimaride-Asp.NetCore

---
+++ MvcWebUI/ValidationRules/FluentValidation/ShippingDetailValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.ValidationRules.FluentValidation
+{
+    public class ShippingDetailValidator : AbstractValidator<ShippingDetail>
+    {
+        public ShippingDetailValidator()
+        {
+            RuleFor(s => s.FirstName).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(s => s.LastName).NotEmpty().WithMessage("Soyad Alanı Boş Geçilemez");
+            RuleFor(s => s.Age).InclusiveBetween(18, 65);
+            RuleFor(s => s.Email).EmailAddress();
+
+        }
+    }
+}
